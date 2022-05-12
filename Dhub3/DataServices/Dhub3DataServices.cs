d698207c4227b8b63491e516a13d33d4cc1bd66b
Fahed Al-Dhubaib <f_aldhubaib@hotmail.com>
2022-05-12 14:07:26 +0300

    added dhub3 project

---
+++ Dhub3/DataServices/Dhub3DataServices.cs
@@ -0,0 +1,34 @@
+﻿using Beep.Winform.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheTechIdea.Beep;
+using TheTechIdea.Util;
+
+namespace Dhub3.DataServices
+{
+    public class Dhub3DataServices
+    {
+       
+        public Dhub3DataServices(IDMEEditor editor)
+        {
+            DMeditor = editor;
+        }
+        public IDMEEditor DMeditor { get; set; }
+        public ResourceManager ResourceManager { get; set; }
+        public IErrorsInfo CreateGetConnection()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                DMeditor.AddLogMessage("Dhub3", $"Could not Get/Create Data Connection {ex.Message}", DateTime.Now, 0, null, Errors.Failed);
+            }
+        }
+
+    }
+}
