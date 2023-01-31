394a36d97999f5dd93facabae1eb85f95fa81143
fahad@thetechidea.com <fahad@thetechidea.com>
2023-01-31 19:38:17 +0300

    update ML .NET. Update Python Default Folders

--- BeepContainer/ContainerManagement/CantainerManager.cs
+++ BeepContainer/ContainerManagement/CantainerManager.cs
@@ -220,7 +220,7 @@ namespace TheTechIdea.Beep.Containers.ContainerManagement
                             //-------- Create DMService -----
                             try
                             {
-                               services.AddScoped<IBeepDMService>(s=>new BeepDMService(pContainer));
+                               services.AddScoped<IBeepDMService>(s=>new BeepMain(pContainer));
                                 var provider = services.BuildServiceProvider();
                                 var ContianerService = provider.GetService<IBeepDMService>();
 
